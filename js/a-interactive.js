ef7b1fe1fead1a3e1dc8f2f7bb2abf074fbd292e
toposcorner <52015719+toposcorner@users.noreply.github.com>
2019-07-03 13:25:47 -0400

    Add files via upload
    interactive image resizing

---
+++ js/a-interactive.js
@@ -0,0 +1,10 @@
+
+//<![CDATA[
+
+    $(window).load(function(){
+      
+$(window).resize(function() {
+    document.getElementById('interactive').style.height= String($(window).height())+'px';
+}).resize();
+
+    });
